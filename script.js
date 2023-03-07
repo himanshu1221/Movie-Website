e8236cd1536f1e18539c8bd2d921758599d39a88
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-03-07 14:31:21 +0530

    formating of the code

--- script.js
+++ script.js
@@ -29,20 +29,20 @@ function returnMovies(url) {
             data.results.forEach(element => {
                 const div_card = document.createElement('div');
                 div_card.setAttribute('class', 'card');
-                
+
                 const div_row = document.createElement('div');
                 div_row.setAttribute('class', 'row');
-                
+
                 const div_column = document.createElement('div');
                 div_column.setAttribute('class', 'column');
-                
+
                 const image = document.createElement('img');
                 image.setAttribute('class', 'thumbnail');
                 image.setAttribute('id', 'image');
-                
+
                 const title = document.createElement('h3');
                 title.setAttribute('id', 'title');
-                
+
                 const center = document.createElement('center');
 
                 // adding dynamic data inside a card 
@@ -60,13 +60,13 @@ function returnMovies(url) {
         });
 };
 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     main.innerHTML = '';
 
     const searchItem = search.value;
-    if (searchItem){
-        returnMovies(SEARCHAPI+searchItem);
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
         search.value = "";
     };
 });
